2b15a800c8aa72203690c11b53ba61d7517ad9ae
BpOHenOe3D <xkotrusx@gmail.com>
2022-10-20 21:00:20 +0300

    api tests

---
+++ src/test/java/com/tricentis/tests/tests/Api.java
@@ -0,0 +1,114 @@
+package com.tricentis.tests.tests;
+
+import com.github.javafaker.Faker;
+import com.tricentis.tests.helpers.AllureRestAssuredFilter;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+public class Api extends TestsConfiguration {
+    Faker faker = new Faker();
+
+    public String
+            firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            password = faker.internet().password();
+    final String tokenName = "__RequestVerificationToken";
+    final String authCookieName = "NOPCOMMERCE.AUTH";
+
+    @Test
+    @DisplayName("Регистрация пользователя через API + проверка через UI")
+    void registrationAndCheckApiAndUi() {
+        Response register =
+                given()
+                        .filter(AllureRestAssuredFilter.withCustomTemplates())
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get("/register")
+                        .then()
+                        .log().all()
+                        .extract()
+                        .response();
+
+        String registryToken = register.htmlPath().getString("**.find{it.@name == '__RequestVerificationToken'}.@value");
+
+        String authorizationCookie =
+                given()
+                        .filter(AllureRestAssuredFilter.withCustomTemplates())
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .formParam("Gender", "M")
+                        .formParam("FirstName", firstName)
+                        .formParam("LastName", lastName)
+                        .formParam("Email", email)
+                        .formParam("Password", password)
+                        .formParam("ConfirmPassword", password)
+                        .formParam(tokenName, registryToken)
+                        .cookies(register.cookies())
+                        .when()
+                        .post("/register")
+                        .then()
+                        .statusCode(302)
+                        .extract()
+                        .cookie(authCookieName);
+
+
+        step("Open minimal content, because cookie can be set when site is opened", () ->
+                open("/Themes/DefaultClean/Content/images/logo.png"));
+
+        step("Set cookie to to browser", () ->
+                getWebDriver().manage().addCookie(
+                        new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+
+        step("Open main page", () ->
+                open(""));
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(email)));
+    }
+
+    @Test
+    @DisplayName("Логин через API + редактирование профиля через UI")
+    void editProfileApiAndUi() {
+        String loginCookie = given()
+                .filter(AllureRestAssuredFilter.withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("Email", "1234@mai.riu")
+                .formParam("Password", "123456")
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(302)
+                .extract()
+                .cookie(authCookieName);
+        step("Open minimal content, because cookie can be set when site is opened", () ->
+                open("/Themes/DefaultClean/Content/images/logo.png"));
+
+        step("Set cookie to to browser", () ->
+                getWebDriver().manage().addCookie(
+                        new Cookie("NOPCOMMERCE.AUTH", loginCookie)));
+
+        step("Open edit profile page", () ->
+                open("/customer/info"));
+        $("#gender-male").click();
+        $("#FirstName").setValue(firstName);
+        $("#LastName").setValue(lastName);
+        $("[value='Save']").click();
+        $("#FirstName").shouldHave(value(firstName));
+        $("#LastName").shouldHave(value(lastName));
+
+
+    }
+}
